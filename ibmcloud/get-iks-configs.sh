caa91b6ecad390e0f2ad0fb293f903fa21a66c4d
John Pape <jpapejr@icloud.com>
2019-05-31 14:58:39 -0400

    Consolidation

--- ibmcloud/get-iks-configs.sh
+++ ibmcloud/get-iks-configs.sh
@@ -4,9 +4,29 @@
 # IBM Cloud Kubernetes Service cluster configuration for each cluster
 # within each region.
 
-# for region in us-east us-south uk-south eu-central ap-north ap-south; do 
-#   ibmcloud ks region-set $region; 
-  for i in `ibmcloud ks clusters -s | awk '!/Name|OK|^ /{print $1}'`; do 
+# CHANGE VALUE
+# Note the value is the IBM ID used to log into ibmcloud. This is the same
+# value that appears in the downloaded IKS cluster config files for the 
+# context user field.
+username="jtpape@us.ibm.com" #set to IBM ID used with ibmcloud login
+
+# IBM Cloud Kubernetes Service cluster config dir
+clustersdir=~/.bluemix/plugins/container-service/clusters
+
+rm -fr $clustersdir
+for i in `ibmcloud ks clusters -s | awk '!/Name|OK|^ /{print $1}'`; do 
     ibmcloud ks cluster-config --export $i | awk -F "=" '{print $NF}'; 
-  done; 
-# done
+done; 
+
+
+
+#Produce a Kubernetes merged config string to be used with the KUBECONF environment variable.
+rm -f "$HOME/.kube/config"
+while IFS= read -r -d '' file
+do 
+    clustername=$(awk '/current-context/{print $2}' < $file)
+    sed -i.bak "/$clustername-$username/!s/$username/$clustername-$username/g" $file && rm $file.bak
+    mergedconfig="$mergedconfig:$file"
+done < <(find $clustersdir -type f -name "*.yml" -print0)
+export KUBECONFIG=$mergedconfig
+kubectl config view > "$HOME/.kube/config"
