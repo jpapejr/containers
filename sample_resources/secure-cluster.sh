85633495067adf21aee00b8e3498249844bd7aa1
John Pape <jpapejr@icloud.com>
2020-07-13 15:44:07 -0400

    Update secure-cluster.sh

--- sample_resources/secure-cluster.sh
+++ sample_resources/secure-cluster.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # Secure IKS/ROKS public and private interfaces
 # References:
 # - https://cloud.ibm.com/docs/containers?topic=containers-network_policies#isolate_workers_public
@@ -13,25 +13,28 @@ if [ -z "$SECURE_REGION" || -z "$CALICO_CONFIG" ]
     echo "Securing target cluster for $SECURE_REGION with calico policies.."
 fi
 
+git clone https://github.com/IBM-Cloud/kube-samples.git /tmp/kube-samples
+cd /tmp/kube-samples
+
 #Public interface policies (with optionals)
-calicoctl apply -f https://github.com/IBM-Cloud/kube-samples/blob/master/calico-policies/public-network-isolation/$SECURE_REGION/allow-egress-pods-public.yaml -c "$CALICO_CONFIG"
-calicoctl apply -f https://github.com/IBM-Cloud/kube-samples/blob/master/calico-policies/public-network-isolation/$SECURE_REGION/allow-ibm-ports-public.yaml -c "$CALICO_CONFIG"
-calicoctl apply -f https://github.com/IBM-Cloud/kube-samples/blob/master/calico-policies/public-network-isolation/$SECURE_REGION/allow-public-service-endpoint.yaml -c "$CALICO_CONFIG"
-calicoctl apply -f https://github.com/IBM-Cloud/kube-samples/blob/master/calico-policies/public-network-isolation/$SECURE_REGION/deny-all-outbound.yaml -c "$CALICO_CONFIG"
-calicoctl apply -f https://github.com/IBM-Cloud/kube-samples/blob/master/calico-policies/public-network-isolation/$SECURE_REGION/dallow-public-services-pods.yaml -c "$CALICO_CONFIG"
-calicoctl apply -f https://github.com/IBM-Cloud/kube-samples/blob/master/calico-policies/public-network-isolation/$SECURE_REGION/allow-public-services.yaml -c "$CALICO_CONFIG"
+calicoctl apply -f calico-policies/public-network-isolation/$SECURE_REGION/allow-egress-pods-public.yaml -c "$CALICO_CONFIG"
+calicoctl apply -f calico-policies/public-network-isolation/$SECURE_REGION/allow-ibm-ports-public.yaml -c "$CALICO_CONFIG"
+calicoctl apply -f calico-policies/public-network-isolation/$SECURE_REGION/allow-public-service-endpoint.yaml -c "$CALICO_CONFIG"
+calicoctl apply -f calico-policies/public-network-isolation/$SECURE_REGION/deny-all-outbound.yaml -c "$CALICO_CONFIG"
+calicoctl apply -f calico-policies/public-network-isolation/$SECURE_REGION/dallow-public-services-pods.yaml -c "$CALICO_CONFIG"
+calicoctl apply -f calico-policies/public-network-isolation/$SECURE_REGION/allow-public-services.yaml -c "$CALICO_CONFIG"
 
 #Private interface policies (with optionals)
-calicoctl apply -f https://github.com/IBM-Cloud/kube-samples/blob/master/calico-policies/private-network-isolation/calico-v3/$SECURE_REGION/allow-all-workers-private.yaml -c "$CALICO_CONFIG"
-calicoctl apply -f https://github.com/IBM-Cloud/kube-samples/blob/master/calico-policies/private-network-isolation/calico-v3/$SECURE_REGION/allow-egress-pods-private.yaml -c "$CALICO_CONFIG"
-calicoctl apply -f https://github.com/IBM-Cloud/kube-samples/blob/master/calico-policies/private-network-isolation/calico-v3/$SECURE_REGION/allow-ibm-ports-private.yaml -c "$CALICO_CONFIG"
-calicoctl apply -f https://github.com/IBM-Cloud/kube-samples/blob/master/calico-policies/private-network-isolation/calico-v3/$SECURE_REGION/allow-icmp-private.yaml -c "$CALICO_CONFIG"
-calicoctl apply -f https://github.com/IBM-Cloud/kube-samples/blob/master/calico-policies/private-network-isolation/calico-v3/$SECURE_REGION/allow-private-service-endpoint.yaml -c "$CALICO_CONFIG"
-calicoctl apply -f https://github.com/IBM-Cloud/kube-samples/blob/master/calico-policies/private-network-isolation/calico-v3/$SECURE_REGION/allow-sys-mgmt-private.yaml -c "$CALICO_CONFIG"
-calicoctl apply -f https://github.com/IBM-Cloud/kube-samples/blob/master/calico-policies/private-network-isolation/calico-v3/$SECURE_REGION/allow-private-services.yaml -c "$CALICO_CONFIG"
-calicoctl apply -f https://github.com/IBM-Cloud/kube-samples/blob/master/calico-policies/private-network-isolation/calico-v3/$SECURE_REGION/allow-private-services-pods.yaml -c "$CALICO_CONFIG"
-calicoctl apply -f https://github.com/IBM-Cloud/kube-samples/blob/master/calico-policies/private-network-isolation/calico-v3/$SECURE_REGION/allow-vrrp-private.yaml -c "$CALICO_CONFIG"
-calicoctl apply -f https://github.com/IBM-Cloud/kube-samples/blob/master/calico-policies/private-network-isolation/calico-v3/$SECURE_REGION/deny-all-private-default.yaml -c "$CALICO_CONFIG"
+calicoctl apply -f calico-policies/private-network-isolation/calico-v3/$SECURE_REGION/allow-all-workers-private.yaml -c "$CALICO_CONFIG"
+calicoctl apply -f calico-policies/private-network-isolation/calico-v3/$SECURE_REGION/allow-egress-pods-private.yaml -c "$CALICO_CONFIG"
+calicoctl apply -f calico-policies/private-network-isolation/calico-v3/$SECURE_REGION/allow-ibm-ports-private.yaml -c "$CALICO_CONFIG"
+calicoctl apply -f calico-policies/private-network-isolation/calico-v3/$SECURE_REGION/allow-icmp-private.yaml -c "$CALICO_CONFIG"
+calicoctl apply -f calico-policies/private-network-isolation/calico-v3/$SECURE_REGION/allow-private-service-endpoint.yaml -c "$CALICO_CONFIG"
+calicoctl apply -f calico-policies/private-network-isolation/calico-v3/$SECURE_REGION/allow-sys-mgmt-private.yaml -c "$CALICO_CONFIG"
+calicoctl apply -f calico-policies/private-network-isolation/calico-v3/$SECURE_REGION/allow-private-services.yaml -c "$CALICO_CONFIG"
+calicoctl apply -f calico-policies/private-network-isolation/calico-v3/$SECURE_REGION/allow-private-services-pods.yaml -c "$CALICO_CONFIG"
+calicoctl apply -f calico-policies/private-network-isolation/calico-v3/$SECURE_REGION/allow-vrrp-private.yaml -c "$CALICO_CONFIG"
+calicoctl apply -f calico-policies/private-network-isolation/calico-v3/$SECURE_REGION/deny-all-private-default.yaml -c "$CALICO_CONFIG"
 
 
 
