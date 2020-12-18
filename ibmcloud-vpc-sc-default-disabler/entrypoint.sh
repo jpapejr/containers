cbf85a94a577d7fb80f818c8d8f6514d685486ac
jpapejr <jpapejr@icloud.com>
2020-12-18 15:13:04 -0500

    bug fixes, better auth

--- ibmcloud-vpc-sc-default-disabler/entrypoint.sh
+++ ibmcloud-vpc-sc-default-disabler/entrypoint.sh
@@ -1,4 +1,4 @@
-/lib/ld-musl-x86_64.so.1 --library-path /lib /usr/local/bin/oc login $SERVER --token $TOKEN
+/lib/ld-musl-x86_64.so.1 --library-path /lib /usr/local/bin/oc login $SERVER -u apikey -p $TOKEN
 while true;
  do /lib/ld-musl-x86_64.so.1 --library-path /lib /usr/local/bin/oc annotate sc/ibmc-vpc-block-10iops-tier storageclass.beta.kubernetes.io/is-default-class="false" --overwrite; sleep 2;
 done
