e7b935beb59ac6f841b31ff0c0198dfe1292de76
John Pape <jpapejr@icloud.com>
2022-10-06 10:05:49 -0400

    Update entrypoint.sh

--- ibmcloud-vpc-sc-default-disabler/entrypoint.sh
+++ ibmcloud-vpc-sc-default-disabler/entrypoint.sh
@@ -1,5 +1,5 @@
 /lib/ld-musl-x86_64.so.1 --library-path /lib /usr/local/bin/oc login $SERVER -u apikey -p $TOKEN
 while true;
- do /lib/ld-musl-x86_64.so.1 --library-path /lib /usr/local/bin/oc annotate sc/ibmc-vpc-block-10iops-tier storageclass.beta.kubernetes.io/is-default-class="false" --overwrite; sleep 2;
+ do /lib/ld-musl-x86_64.so.1 --library-path /lib /usr/local/bin/oc annotate sc/ibmc-vpc-block-10iops-tier storageclass.kubernetes.io/is-default-class="false" --overwrite; sleep 2;
 done
 
